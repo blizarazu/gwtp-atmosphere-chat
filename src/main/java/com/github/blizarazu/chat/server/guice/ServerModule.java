6c2ab65373616d5ca66f5294dbc964e232a7331a
blizarazu <blizarazu@gmail.com>
2014-08-11 21:07:05 +0200

    Create GWT module

---
+++ src/main/java/com/github/blizarazu/chat/server/guice/ServerModule.java
@@ -0,0 +1,29 @@
+/*
+ * 
+ * Copyright (c) 2014 blizarazu.
+ *
+ * This file is part of gwtp-atmosphere-chat.
+ *
+ * gwtp-atmosphere-chat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * gwtp-atmosphere-chat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gwtp-atmosphere-chat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.github.blizarazu.chat.server.guice;
+
+import com.gwtplatform.dispatch.server.guice.HandlerModule;
+
+public class ServerModule extends HandlerModule {
+    @Override
+    protected void configureHandlers() {
+    }
+}
